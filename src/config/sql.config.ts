afb2c436c823086c00cad86839f8d60f6bec9e1f
Marat <evolution99@rambler.ru>
2023-06-25 22:18:21 +0300

    Edited sql.config.ts in config folder

--- src/config/sql.config.ts
+++ src/config/sql.config.ts
@@ -3,8 +3,7 @@ import { Dialect } from 'sequelize';
 
 export const sqlConfig = registerAs('database', () => ({
   dialect: <Dialect>process.env.SQL_DIALECT || 'mysql',
-  logging: process.env.SQL_LOGGING === 'true',
-  true: false,
+  logging: process.env.SQL_LOGGING === 'true' ? true : false,
   host: process.env.DATABASE_HOST,
   port: +process.env.DATABESE_PORT,
   username: process.env.DATABASE_USER,
