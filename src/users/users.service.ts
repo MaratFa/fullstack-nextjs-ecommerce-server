e3a95d5f5ca2b54a5dfb0eb9efe0df15272d9cd6
Marat <evolution99@rambler.ru>
2023-06-25 22:39:06 +0300

    Edited users.service.ts in users folder

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { User } from './users.model';
+import { InjectModel } from '@nestjs/sequelize';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    constructor(
+        @InjectModel(User)
+        private userModel: typeof User,
+    ) {}
+
+    findOne(filter: {
+        where: { id?: string; username?: string; email?: string };
+    }): Promise<User> {
+        return this.userModel.findOne({ ...filter });
+    }
+
+    async create(createUserDto: Promise<User | {warningMessage: string}> {
+
+    }
+}
