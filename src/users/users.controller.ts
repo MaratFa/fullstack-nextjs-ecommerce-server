03c2f4d362f5f98225cf3ee40446291c87504a13
Marat <evolution99@rambler.ru>
2023-06-27 20:46:26 +0300

    Edited users.service.ts in users folder

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,13 +1,18 @@
 import {
   Body,
   Controller,
+  Get,
   Header,
   HttpCode,
   HttpStatus,
   Post,
+  Request,
+  UseGuards,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
+import { AuthenticatedGuard } from 'src/auth/authenticated.guard';
+import { LocalAuthGuard } from '../auth/local.auth.guard';
 
 @Controller('users')
 export class UsersController {
@@ -18,5 +23,18 @@ export class UsersController {
   @Header('Content-Type', 'application/json')
   createUser(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
+  }
+
+  @Post('/login')
+  @UseGuards(LocalAuthGuard)
+  @HttpCode(HttpStatus.OK)
+  login(@Request() req) {
+    return { user: req.user, msg: 'Logged in' };
+  }
+
+  @Get('/login-check')
+  @UseGuards(AuthenticatedGuard)
+  loginCheck(@Request() req) {
+    return req.user;
   }
 }
